fcf15871136b50e5524228f42b9f09e70f091e4a
tranminhnhut1999 <tranminhnhut852@gmail.com>
2023-06-22 20:08:50 +0700

    5,6

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
-// Tập hợp các số được thêm 
+// Tập hợp các số được thêm
 var listNumber = [];
 function themSo() {
-    var number = document.getElementById("txtNumber").value * 1;
-    listNumber.push(number);
-    var result = "";
-    for (var i = 0; i < listNumber.length; i++)
-        result += listNumber[i] + " ";
-    document.getElementById("listNumber").innerHTML = result;
+  var number = document.getElementById("txtNumber").value * 1;
+  listNumber.push(number);
+  var result = "";
+  for (var i = 0; i < listNumber.length; i++) result += listNumber[i] + " ";
+  document.getElementById("listNumber").innerHTML = result;
 }
 /**
  * Câu 1: Tổng số dương
- * 1.Tạo biến tích luỹ số dương 
+ * 1.Tạo biến tích luỹ số dương
  * 2. Duyệt mảng listNumber
  * 3. Kiểm tra phần tử có là số dương ( listNumber[i] > 0)
- *      => True => Tích luý phần tử 
+ *      => True => Tích luý phần tử
  * 4.Show kết quả #tongSoDuong
  */
 function tSoDuong() {
-    var total = 0;
-    for (var i = 0; i < listNumber.length; i++) {
-        if (listNumber[i] >= 0) {
-            total += listNumber[i];
-        }
+  var total = 0;
+  for (var i = 0; i < listNumber.length; i++) {
+    if (listNumber[i] >= 0) {
+      total += listNumber[i];
     }
-    var result = "Tổng số dương: " + total;
-    document.getElementById("tongSoDuong").innerHTML = result;
+  }
+  var result = "Tổng số dương: " + total;
+  document.getElementById("tongSoDuong").innerHTML = result;
 }
-
-
 /**
  * Câu 2: Đếm số âm
  * 1. Tạo biến tích luỹ số âm dem=0
@@ -37,14 +34,14 @@ function tSoDuong() {
  * 4.Show kết quả #demSoDuong
  */
 function dSoDuong() {
-    var dem = 0;
-    for (var i = 0; i < listNumber.length; i++) {
-        if (listNumber[i] >= 0) {
-            dem++;
-        }
+  var dem = 0;
+  for (var i = 0; i < listNumber.length; i++) {
+    if (listNumber[i] >= 0) {
+      dem++;
     }
-    var result = "Có " + dem + " số dương";
-    document.getElementById("demSoDuong").innerHTML = result;
+  }
+  var result = "Có " + dem + " số dương";
+  document.getElementById("demSoDuong").innerHTML = result;
 }
 
 /**
@@ -57,14 +54,14 @@ function dSoDuong() {
  */
 
 function sNhoNhat() {
-    var min = listNumber[0];
-    for (var i = 1; i < listNumber.length; i++) {
-        if (min < listNumber[i]) {
-            listNumber[i];
-        }
+  var min = listNumber[0];
+  for (var i = 1; i < listNumber.length; i++) {
+    if (min < listNumber[i]) {
+      listNumber[i];
     }
-    var result = "Số nhỏ nhất: " + min
-    document.getElementById("sNhoNhat").innerHTML = result;
+  }
+  var result = "Số nhỏ nhất: " + min;
+  document.getElementById("sNhoNhat").innerHTML = result;
 }
 
 /**
@@ -82,54 +79,75 @@ function sNhoNhat() {
  */
 
 function sdNhoNhat() {
-    var sDuong = listNumber[0];
-    for (var i = 0; i < listNumber.length; i++) {
-        if (listNumber[i] >= 0) {
-            sDuong += listNumber[i];
-        }
-
+  var sDuong = [];
+  for (var i = 0; i < listNumber.length; i++) {
+    if (listNumber[i] > 0) {
+      sDuong.push(listNumber[i]);
     }
-    var min = sDuong[0];
-    for (var i = 1; i < sDuong.length; i++) {
-        if (min < sDuong[i]) {
-            sDuong[i];
-        }
+  }
+  var min = sDuong[0];
+  for (var j = 1; j < sDuong.length; j++) {
+    if (sDuong[i] < min) {
+      min[j];
     }
-    console.log(min);
-    var result = "Số dương nhỏ nhất: " + min;
-    document.getElementById("sdNhoNhat").innerHTML = result;
+  }
+  console.log(min);
+  var result = "Số dương nhỏ nhất: " + min;
+  document.getElementById("sdNhoNhat").innerHTML = result;
 }
 
+/**
+ * Câu 5: Tìm số chẵn cuối cùng
+ * 1. tạo biến sChan để chứa số chẵn trong listNumber
+ * 2. Duyệt mảng sChan
+ * 3. Kiểm tra số chẵn trong listNumber cuối? (listNumber[i] % 2 === 0)
+ *      => true => Cập nhật mới cho sChan
+ * 4. Show kết quả số chẵn cuối
+ */
+function soChanCuoi() {
+  var sChan = 0;
+  for (var i = 0; i < listNumber.length; i++) {
+    if (listNumber[i] % 2 === 0) {
+      sChan = listNumber[i];
+    }
+  }
+  var result = "Số chẵn cuối: " + sChan;
+  document.getElementById("soChanCuoi").innerHTML = result;
+}
+/**
+ * Câu 6 : Đổi chỗ
+ */
+function doiCho() {
+  for (var i = 0; i < listNumber.length; i++) {
+    for (var j = 0; j < listNumber.length; j++) {
+      if (listNumber[i] > listNumber[j]) {
+        var temp = listNumber[i];
+        listNumber[i] = listNumber[j];
+        listNumber[j] = temp;
+      }
+    }
+  }
+  var result = "Đổi chỗ: " + listNumber;
+  document.getElementById("doiCho").innerHTML = result;
+}
 
 /**
  * Câu 7: Sắp xếp tăng dần
  */
 function sXTangDan() {
-    for (var i = 0; i < listNumber.length - 1; i++) {
-        for (var j = i + 1; j < listNumber.length; j++) {
-            if (listNumber[i] > listNumber[j]) {
-                var temp = listNumber[i];
-                listNumber[i] = listNumber[j];
-                listNumber[j] = temp
-            }
-        }
+  for (var i = 0; i < listNumber.length - 1; i++) {
+    for (var j = i + 1; j < listNumber.length; j++) {
+      if (listNumber[i] > listNumber[j]) {
+        var temp = listNumber[i];
+        listNumber[i] = listNumber[j];
+        listNumber[j] = temp;
+      }
     }
-    var result = "Xếp theo tăng dần: " + listNumber;
-    document.getElementById("sXTangDan").innerHTML = result;
-};
+  }
+  var result = "Xếp theo tăng dần: " + listNumber;
+  document.getElementById("sXTangDan").innerHTML = result;
+}
 
 /**
  * Câu 8 : Tìm số nguyên đầu tiên
  */
-//function soDT() {
-    //var result = ""
-    //switch (listNumber) {
-        //case 1:
-            //result = "Số đầu tiên"
-            //break;
-        //default
-            //result = "Số đầu tiên " + -1
-
-    //}
-    //document.getElementById("sDauTien").innerHTML = result;
-//}
